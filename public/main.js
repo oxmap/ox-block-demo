2413552937b3daa6a7e3fa7d250eee5fc7ccdfab
SavinAO <SavinAO@tomskasu.ru>
2020-12-03 14:57:16 +0700

    Добавлен scale.

--- public/main.js
+++ public/main.js
@@ -1,58 +1,93 @@
 // Orientaion
-function getBrowser() {
-    const agent = window.navigator.userAgent.toLowerCase()
-    switch (true) {
-      case agent.indexOf('chrome') > -1 && !!window.chrome:
-        return 'chrome';
-      case agent.indexOf('safari') > -1:
-        return 'safari';
-      default:
-        return undefined;
-    }
+function getMobileOS() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  // Windows Phone must come first because its UA also contains "Android"
+  if (/windows phone/i.test(userAgent)) {
+    return "unknown";
+  }
+
+  if (/android/i.test(userAgent)) {
+    return "android";
+  }
+
+  // iOS detection from: http://stackoverflow.com/a/9039885/177710
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    return "ios";
+  }
+
+  return "unknown";
 }
- 
+
 const browsers = {
-    safari: {
-        '180': 'orient90', 
-        '90': 'orient0',
-        '0': 'orient90',
-        '-90': 'orient0'
-    },
-    chrome: {
-        '180': 'orient0', 
-        '90': 'orient90',
-        '0': 'orient0',
-        '-90': 'orient90'
-    },
-    undefined: {
-        '180': 'orient0', 
-        '90': 'orient90',
-        '0': 'orient0',
-        '-90': 'orient90'
-    }
-}
+  ios: {
+    180: "orient90",
+    90: "orient0",
+    0: "orient90",
+    "-90": "orient0",
+  },
+  android: {
+    180: "orient0",
+    90: "orient90",
+    0: "orient0",
+    "-90": "orient90",
+  },
+  unknown: {
+    180: "orient0",
+    90: "orient90",
+    0: "orient0",
+    "-90": "orient90",
+  },
+};
 
 function setClass() {
-    document.getElementsByClassName('text')[0].textContent=`Угол ориентации = ${window.orientation}`;
-    document.getElementsByTagName('body')[0].className = browsers[getBrowser()][window.orientation];
+  document.getElementsByClassName(
+    "text"
+  )[0].textContent = `Угол ориентации = ${window.orientation}`;
+  document.body.className =browsers[getMobileOS()][window.orientation];
 }
 
-window.addEventListener('orientationchange', function () {
+window.addEventListener(
+  "orientationchange",
+  function () {
     setClass();
-}, true);
+  },
+  true
+);
 
 setClass();
 
 // Scale
-window.addEventListener('keydown', function (event) {
-    if (event.ctrlKey==true && (event.which == '61' || event.which == '107' || event.which == '173' || event.which == '109'  || event.which == '187'  || event.which == '189'  ) ) {
-        event.preventDefault();
-     }
-}, true)
-
-window.addEventListener('wheel', function (event) {
-    if (event.ctrlKey == true) {
-        event.preventDefault();
-        return false;
+window.addEventListener(
+  "keydown",
+  function (event) {
+    if (
+      event.ctrlKey == true &&
+      (event.which == "61" ||
+        event.which == "107" ||
+        event.which == "173" ||
+        event.which == "109" ||
+        event.which == "187" ||
+        event.which == "189")
+    ) {
+      event.preventDefault();
     }
-}, {passive: false});
+  },
+  true
+);
+
+window.addEventListener(
+  "wheel",
+  function (event) {
+    if (event.ctrlKey == true) {
+      event.preventDefault();
+      return false;
+    }
+  },
+  { passive: false }
+);
+
+window.visualViewport.addEventListener("resize", viewportHandler);
+function viewportHandler() {
+    document.body.style.transform = `scale(${1/devicePixelRatio})`;
+}
